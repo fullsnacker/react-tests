f6f2e2871a5159cfab512c14fc6f56d48166c147
fullsnacker <fullsnacker@gmail.com>
2023-12-14 16:56:34 -0300

    Modified project for tests

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { CounterApp } from './CounterApp';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { FirstApp } from './FirstApp';
+// import { HelloWorldApp } from './HelloWorldApp';
+import './styles.css';
+
+
+
+ReactDOM.createRoot( document.getElementById('root') ).render(
+    <React.StrictMode>
+        <CounterApp value={ 20 } />
+        {/* <FirstApp title="Hola, Soy Vegeta" /> */}
+    </React.StrictMode>
+);
+
+
