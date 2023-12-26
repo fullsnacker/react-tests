af5c30fce38380922d58fa9bbdbc2223241b3f81
fullsnacker <fullsnacker@gmail.com>
2023-12-26 10:19:24 -0300

    testing by id and text

--- tests/FirstApp.test.jsx
+++ tests/FirstApp.test.jsx
@@ -2,27 +2,37 @@ import { render } from "@testing-library/react"
 import { FirstApp } from "../src/FirstApp"
 
 describe('Pruebas en FirstApp', () => { 
-    test('debe hacer match con el snapshot', () => { 
+    // test('debe hacer match con el snapshot', () => { 
 
-        const title = 'HOLA';
-        const {container} = render( <FirstApp title={ title }/> );
+    //     const title = 'HOLA';
+    //     const {container} = render( <FirstApp title={ title }/> );
 
-        expect( container ).toMatchSnapshot();
+    //     expect( container ).toMatchSnapshot();
 
-    })
+    // })
 
     test('debe de mostrar el titulo en un h1', () => { 
 
         const title = 'HOLA';
-        const {container, getByText} = render( <FirstApp title={ title }/> );
+        const {container, getByText, getByTestId} = render( 
+            <FirstApp 
+                title={ title }
+            /> 
+        );
         
         expect( getByText(title) ).toBeTruthy();
 
-        const h1 = container.querySelector('h1');
+        // const h1 = container.querySelector('h1');
 
-        console.log(h1.innerHTML);
+        // console.log(h1.innerHTML);
 
-        expect( h1.innerHTML ).toContain( title );
+        // expect( h1.innerHTML ).toContain( title );
+
+        expect( getByTestId('test-title') ).toBeTruthy();
+
+        expect( getByTestId('test-title').innerHTML ).toContain(title);
+
+
           
     })
 })
