7c17e01ff5c45d52ae6efa01002102e7c2cde213
fullsnacker <fullsnacker@gmail.com>
2023-12-26 10:02:54 -0300

    test with snapshots

--- tests/FirstApp.test.jsx
+++ tests/FirstApp.test.jsx
@@ -3,7 +3,26 @@ import { FirstApp } from "../src/FirstApp"
 
 describe('Pruebas en FirstApp', () => { 
     test('debe hacer match con el snapshot', () => { 
-        const title = 'HOLA'
-         render( <FirstApp title={title}/>)
+
+        const title = 'HOLA';
+        const {container} = render( <FirstApp title={ title }/> );
+
+        expect( container ).toMatchSnapshot();
+
+    })
+
+    test('debe de mostrar el titulo en un h1', () => { 
+
+        const title = 'HOLA';
+        const {container, getByText} = render( <FirstApp title={ title }/> );
+        
+        expect( getByText(title) ).toBeTruthy();
+
+        const h1 = container.querySelector('h1');
+
+        console.log(h1.innerHTML);
+
+        expect( h1.innerHTML ).toContain( title );
+          
     })
 })
